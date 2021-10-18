cfb68be597135b0bdc47861bc1e6261764be89e6
Julius <julius.simonelli@gmail.com>
2021-10-17 22:52:56 -0700

    more clean up

--- src/cv_dataclass/plot_metadata.py
+++ src/cv_dataclass/plot_metadata.py
@@ -12,20 +12,20 @@ def plot_histo(lst, max_padding=0.1):
     fig.set_size_inches(12, 12)
     bins = np.arange(0, max(lst) + max_padding, 0.1)
     plt.hist(lst, bins=bins, rwidth=0.85)
-    plt.grid(axis='y', alpha=0.75)
-    plt.xlabel('GSD (in meters)')
+    plt.grid(axis="y", alpha=0.75)
+    plt.xlabel("GSD (in meters)")
     plt.xticks(rotation=45)
-    plt.savefig('test.png')
+    plt.savefig("test.png")
 
 
 def plot_boxplot(lst):
     plt.boxplot(lst, vert=False)
-    plt.savefig('boxplot.png')
+    plt.savefig("boxplot.png")
 
 
 if __name__ == "__main__":
 
-    with open('gsd.pkl', 'rb') as f:
+    with open("gsd.pkl", "rb") as f:
         gsd = pickle.load(f)
 
     plot_histo(gsd)
